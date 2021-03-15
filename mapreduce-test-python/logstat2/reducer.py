b7c2d1816582f249f5096e74c9d5e59df4df6d4d
wangyepu888 <34576117+wangyepu888@users.noreply.github.com>
2021-03-15 11:11:10 +0800

    Update reducer.py

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -16,5 +16,15 @@ for line in sys.stdin:
 
 
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
-for ip, count in sorted_dict_ip_count:
-    print '%s\t%s' % (ip, count)
+dic={}
+L=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for ip, count in sorted_dict_ip_count:   
+    for i in range(24):
+       j='0'+str(i)
+       if ip[1]==j[-2]&ip[2]==j[-1]&L[i]<4:
+          dic[ip]=count
+          L[i]=L[i]+1
+    
+for ip, count in dic:
+    print ('%s\t%s' % (ip, count))
